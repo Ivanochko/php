2dfec9d4943da346943d81cf9a0a5a548e3a3cad
Ivanochko <vasa.ivanochko.17@gmail.com>
2021-04-01 21:33:36 +0300

    commit

--- htdocs/Web/lab7/ivanochko_lab7_example_4.php
+++ htdocs/Web/lab7/ivanochko_lab7_example_4.php
@@ -1,15 +1,37 @@
 <html>
 
 <head>
-    <?php require("../config.php") ?>
+    <?php require("../config.php");
+    class User
+    {
+        public $surname;
+        public $name;
+        public $age;
+        public $email;
+
+        function __construct(
+            string $surname,
+            string $name,
+            string $age,
+            string $email
+        ) {
+            $this->surname = $surname;
+            $this->name = $name;
+            $this->age = $age;
+            $this->email = $email;
+        }
+    }
+
+    ?>
 </head>
 
 <body>
     <div class="main">
-        <h1 class="header">Завдання #1</h1>
+        <h1 class="header">Завдання #4</h1>
         <div class="result">
-
             <?php
+
+
 
             ?>
         </div>
