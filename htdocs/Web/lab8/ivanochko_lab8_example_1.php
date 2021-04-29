c9d52a4b6c3f95f7ec91e6cdde1834b71864489b
Ivanochko <vasa.ivanochko.17@gmail.com>
2021-04-29 21:08:53 +0300

    oh no, there is comments

--- htdocs/Web/lab8/ivanochko_lab8_example_1.php
+++ htdocs/Web/lab8/ivanochko_lab8_example_1.php
@@ -14,18 +14,24 @@
 
             $mysqli = open_connection();
 
+            // Видалення таблиця
             mysqli_query($mysqli, "DROP TABLE dusc");
+            // Створення таблиці заново
             mysqli_query($mysqli, "CREATE TABLE dusc(id INTEGER PRIMARY KEY AUTO_INCREMENT,name_d VARCHAR(200) UNIQUE ,key_concepts TEXT)");
+            // Вставлення даних в таблицю
             mysqli_query($mysqli, "INSERT into dusc (name_d, key_concepts) values ('Основи інтернет','HTML, CSS, Java script, WWWW')");
             mysqli_query($mysqli, "INSERT into dusc (name_d, key_concepts) values ('PHP','Пролог, Моделювання')");
             mysqli_query($mysqli, "INSERT into dusc (name_d, key_concepts) values ('JAVA','OOP, MVC, Hibernate')");
             $name = 'PHP';
             $key1 = 'Database, echo, arrays, objects';
+            // Зміна даних в таблиці
             $res1 = mysqli_query($mysqli, "UPDATE dusc set key_concepts='$key1' where name_d='$name'");
             echo "<i>Вибірка з таблиці dusc<p></i>";
             $k = 1;
+            // Виконання запиту в таблиці
             $result = mysqli_query($mysqli, "SELECT id,name_d, key_concepts FROM dusc");
             echo "<table>";
+            // Вивід даних запиту
             echo "<tr><td>ID</td><td> Назва лекції </td><td> Ключовi поняття </td></tr>";
             while ($row = mysqli_fetch_row($result)) {
                 $id[$k] = $row[0];
@@ -33,6 +39,7 @@
                 $k++;
             }
             echo "</table>";
+            // Закриття з'єднання
             mysqli_close($mysqli);
             ?>
         </div>
